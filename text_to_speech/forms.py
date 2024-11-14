0f0e8af0b0206b896d47fe0cbbe41f04c27e08da
Linus Johansson <golfarenlj@gmail.com>
2024-11-14 18:55:26 +0000

    Rename form field from 'form_input' to 'text_input' for clarity and update label.

--- text_to_speech/forms.py
+++ text_to_speech/forms.py
@@ -1,8 +1,5 @@
 from django import forms
 
-
 class InputForm(forms.Form):
     #Class to create a text field for a user to enter text.
-main
-    form_input = forms.CharField(max_length = 1000)
-
+    text_input = forms.CharField(label='Text Input', max_length=100)
