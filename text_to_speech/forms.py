002cc5d091c5f7165acb03f96e81bbbadf2a6eb3
Ed Radcliffe <ewradcliffe@yahoo.com>
2024-11-14 18:27:48 +0000

    Add input form to text_to_speech.html & view.

--- text_to_speech/forms.py
+++ text_to_speech/forms.py
@@ -3,4 +3,4 @@ from django import forms
 
 class InputForm(forms.Form):
     #Class to create a text field for a user to enter text.
-    form_input = forms.TextField()
+    form_input = forms.CharField(max_length = 1000)
--- text_to_speech/views.py
+++ text_to_speech/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .forms import InputForm
+import assemblyai as aai
 
 
 def text_to_speech(request):
-    return render(request, 'text_to_speech/text_to_speech.html')
+    context ={}
+    context['form']= InputForm()
+    return render(request, "text_to_speech/text_to_speech.html", context)
