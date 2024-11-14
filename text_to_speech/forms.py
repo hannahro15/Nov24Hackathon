a2f6da185cf449c56e584060729e80a79f92d495
Linus Johansson <159924955+j0hanz@users.noreply.github.com>
2024-11-15 00:05:53 +0100

    Add text-to-speech functionality with API endpoint (#54)
    * Add gTTS
    
    * Change the name to scripts.js
    
    * Add text-to-speech functionality with voice selection
    
    * Update text input field to use a textarea widget in InputForm
    
    * Add new URL path for text-to-speech API endpoint
    
    * Add text-to-speech API endpoint with voice selection and error handling
    
    * Fix typo in title, replace submit input with button elements

--- text_to_speech/forms.py
+++ text_to_speech/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 
+
 class InputForm(forms.Form):
-    #Class to create a text field for a user to enter text.
-    text_input = forms.CharField(label='Text Input', max_length=100)
+    # Class to create a text field for a user to enter text.
+    text_input = forms.CharField(
+        label='Text Input', max_length=100, widget=forms.Textarea
+    )
--- text_to_speech/urls.py
+++ text_to_speech/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('text_to_speech/', views.text_to_speech, name='text_to_speech'),
-    
+    path(
+        'text_to_speech_api/',
+        views.text_to_speech_api,
+        name='text_to_speech_api',
+    ),
 ]
