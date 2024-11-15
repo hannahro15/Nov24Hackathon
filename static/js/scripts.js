aba193eb017bb3e18778aea686a2fe29b3e97502
Linus Johansson <159924955+j0hanz@users.noreply.github.com>
2024-11-15 14:30:32 +0100

    Add Support for New Voice Options and UI improvments (#66)
    * Add form styling and add hover effects for voice selection cards
    
    * Refactor voice selection logic and update default voice to UK English
    
    * Enhance text input field with placeholder and styling attributes
    
    * Add support for additional voices and TLD mappings
    
    * Refactor voice options and text input layout.

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,26 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let selectedVoice = "en"; // Default voice
+  let selectedVoice = "en-uk"; // Default voice
 
-  const voiceOptions = document.querySelectorAll(".voice-option");
+  // Get the voice options, text input element, and buttons
+  const voiceOptions = document.querySelectorAll(".card-voice");
   const textInputElement = document.querySelector('[name="text_input"]');
-  const sendButton = document.getElementById("app-buttons-send");
-  const clearButton = document.getElementById("app-buttons-clear");
+  const sendButton = document.getElementById("send-button");
+  const clearButton = document.getElementById("clear-button");
 
-  // Highlight the default selected voice option
-  const defaultVoiceOption = document.querySelector(
-    `.voice-option[data-voice="${selectedVoice}"]`
-  );
-  if (defaultVoiceOption) {
-    defaultVoiceOption.classList.add("selected");
+  // Function to update selected voice option styling
+  function updateSelectedVoice(option) {
+    voiceOptions.forEach((opt) => opt.classList.remove("selected"));
+    option.classList.add("selected");
+    selectedVoice = option.getAttribute("data-voice");
   }
 
-  // Handle voice option selection
+  // Initialize the default selected voice
+  const defaultVoiceOption = document.querySelector(
+    `.card-voice[data-voice="${selectedVoice}"]`
+  );
+  if (defaultVoiceOption) {
+    updateSelectedVoice(defaultVoiceOption);
+  }
+
+  // Add click event listeners to each voice option
   voiceOptions.forEach((option) => {
-    option.addEventListener("click", () => {
-      voiceOptions.forEach((opt) => opt.classList.remove("selected"));
-      option.classList.add("selected");
-      selectedVoice = option.getAttribute("data-voice");
-    });
+    option.addEventListener("click", () => updateSelectedVoice(option));
   });
 
   // Handle 'Send' button click
@@ -31,9 +35,10 @@ document.addEventListener("DOMContentLoaded", () => {
       return;
     }
 
-    sendButton.disabled = true; // Prevent multiple clicks
+    sendButton.disabled = true;
 
     try {
+      // Fetch the text-to-speech API
       const response = await fetch(
         `/text_to_speech_api/?text=${encodeURIComponent(
           textInput
@@ -42,8 +47,11 @@ document.addEventListener("DOMContentLoaded", () => {
       if (!response.ok) {
         throw new Error("Network response was not ok.");
       }
+      // Play the audio response
       const blob = await response.blob();
+      // Create a URL for the blob
       const url = URL.createObjectURL(blob);
+      // Create an audio element and play the audio
       const audio = new Audio(url);
       audio.play();
     } catch (error) {
--- text_to_speech/forms.py
+++ text_to_speech/forms.py
@@ -4,5 +4,13 @@ from django import forms
 class InputForm(forms.Form):
     # Class to create a text field for a user to enter text.
     text_input = forms.CharField(
-        label='Text Input', max_length=100, widget=forms.Textarea
+        label='Text Input',
+        max_length=100,
+        widget=forms.Textarea(
+            attrs={
+                'placeholder': 'Paste your text here...',
+                'class': 'form-control',
+                'rows': 15,
+            },
+        ),
     )
