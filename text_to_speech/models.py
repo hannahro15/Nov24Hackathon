5b6eac9794fead59a9e9961a7df7e5594306e533
Linus Johansson <golfarenlj@gmail.com>
2024-11-16 20:17:00 +0000

    Add Profile model to manage user preferred language and automate profile creation

--- text_to_speech/models.py
+++ text_to_speech/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from gtts.lang import tts_langs
 
-# Create your models here.
+# Get the list of available languages for choices
+LANGUAGE_CHOICES = [(code, name) for code, name in tts_langs().items()]
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    preferred_language = models.CharField(
+        max_length=10,
+        choices=LANGUAGE_CHOICES,
+        default='en',
+    )
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+
+# Signal to create or update Profile whenever User is saved
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
