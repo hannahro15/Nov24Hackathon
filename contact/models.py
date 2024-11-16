2139a511e8d1e3eaa198c572a49ace7b8d2ee6c5
Hannah Olbrich <hannaholbrich@gmail.com>
2024-11-16 13:56:06 +0000

    Alter FAQ model slightly

--- contact/models.py
+++ contact/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Faq(models.Model):
     question = models.CharField(max_length=100)
-    answer = models.CharField(max_length=250)
+    answer = models.TextField(max_length=250)
 
     def __str__(self):
         return self.question
