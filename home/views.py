60a1a3a8415be04bec6ed4926a2abfbcbd234bf2
Linus Johansson <golfarenlj@gmail.com>
2024-11-17 22:49:26 +0000

    Redirect logged in users to text to speech page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+
 
 def home(request):
+    # Redirect to the text_to_speech page if the user is authenticated
+    if request.user.is_authenticated:
+        return redirect('text_to_speech')
     return render(request, 'home/index.html')
